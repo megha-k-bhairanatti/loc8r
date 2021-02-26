e1b7096563778331ced7d9497360319a4ce11dfa
megha-k-bhairanatti <meghakb06@gmail.com>
2021-02-26 18:25:16 -0500

    initial commit

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+
+
+const locationsListByDistance = (req, res) => {
+    res.status(200).json({"status" : "success"});
+};
+const locationsCreate = (req, res) => {
+    res.status(201).json({"status" : "success"});
+};
+
+const locationsReadOne = (req, res) => {
+
+    Loc.findById(req.params.locationid).exec((err, location) => {
+        if(!location){
+            return res.status(404).json({
+                "message": "location not found"
+            });
+        } else if (err) {
+            return res.status(404).json(err);
+        }
+
+        res.status(200).json(location);
+        console.log("findById complete");
+    });    
+};
+
+const locationsUpdateOne = (req, res) => {
+    res.status(200).json({"status" : "success"});
+};
+
+const locationsDeleteOne = (req, res) => {
+    res.status(200).json({"status" : "success"});
+};
+
+module.exports = {
+    locationsListByDistance,
+    locationsCreate,
+    locationsReadOne,
+    locationsUpdateOne,
+    locationsDeleteOne
+}
